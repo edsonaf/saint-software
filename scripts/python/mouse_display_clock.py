9f3cfab72dbca077e18a3fd6b30121f516d2385d
edson <edson@yangasportswater.com>
2025-04-14 10:58:26 +0200

    refactor mouse clock display

--- scripts/python/mouse_display_clock.py
+++ scripts/python/mouse_display_clock.py
@@ -9,14 +9,16 @@ time_event = 'TIME'
 bzzz_event = 'BZZZ'
 
 corePropsPath = '/ProgramData/SteelSeries/SteelSeries Engine 3/coreProps.json'
-gamesense_url = json.load(open(corePropsPath))['address']
+
+with open(corePropsPath, 'r') as file:
+    gamesense_url = json.load(file)['address']
 
 def register_clock():
     clock_metadata = {
         'game': app,
         'game_display_name': display_name,
     }
-    r = requests.post('http://'+gamesense_url+'/game_metadata', json=clock_metadata)
+    requests.post(f'http://{gamesense_url}/game_metadata', json=clock_metadata)
 
 def bind_clock_event():
     clock_handler = {
@@ -38,7 +40,7 @@ def bind_clock_event():
             }
         ]
     }
-    r = requests.post('http://'+gamesense_url+'/bind_game_event', json=clock_handler)
+    requests.post(f'http://{gamesense_url}/bind_game_event', json=clock_handler)
 
 def bind_bzzz_event():
     clock_handler = {
@@ -57,7 +59,7 @@ def bind_bzzz_event():
             }
         ]
     }
-    r = requests.post('http://'+gamesense_url+'/bind_game_event', json=clock_handler)
+    requests.post(f'http://{gamesense_url}/bind_game_event', json=clock_handler)
 
 def send_time(time):
     event_data = {
@@ -65,7 +67,7 @@ def send_time(time):
         'event': time_event,
         'data': { 'value': time }
     }
-    r = requests.post('http://'+gamesense_url+'/game_event', json=event_data)
+    requests.post(f'http://{gamesense_url}/game_event', json=event_data)
 
 def send_bzzz(id):
     event_data = {
@@ -73,7 +75,7 @@ def send_bzzz(id):
         'event': bzzz_event,
         'data': { 'value': id }
     }
-    r = requests.post('http://'+gamesense_url+'/game_event', json=event_data)
+    requests.post(f'http://{gamesense_url}/game_event', json=event_data)
 
 register_clock()
 bind_clock_event()
@@ -83,26 +85,10 @@ bind_bzzz_event()
 def show_time():
     while True:
         now = datetime.datetime.now()
+        print(f"{now.hour}:{now.minute}:{now.second}")
         send_time(now.strftime("%X"))
         if not now.minute and not now.second:
             send_bzzz(now.hour)
         sleep(0.1)
 
-def show_counter(count_down):
-    end_time = datetime.datetime.now() + datetime.timedelta(minutes=int(count_down))
-    while datetime.datetime.now() < end_time:
-        remaining_time = end_time - datetime.datetime.now()
-        total_seconds = int(remaining_time.total_seconds())
-        # Calculate hours, minutes, and seconds
-        hours, remainder = divmod(total_seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        # Format into "HH:MM:SS" string
-        time_str = f"{hours:02}:{minutes:02}:{seconds:02}"
-        print(f"Time remaining: {remaining_time}")
-        send_time(time_str)
-        sleep(1)
-    # we reached zero
-    send_bzzz(0)
-    # print("Countdown reached zero. Exiting.")
-
-show_counter(50)
+show_time()
