e6f33a6f7fe4f40f7f4f4c22667a6da00d17974f
edson <edson@yangasportswater.com>
2025-08-11 21:58:29 +0200

    handled first 9 chapters of tutorial add animated sprites

---
+++ DungeonSlime/Game1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SaintGameLibrary;
+using SaintGameLibrary.Graphics;
+
+namespace DungeonSlime;
+
+public class Game1 : Core
+{
+    private const string GameName = "Dungeon Slime";
+    private AnimatedSprite _slime;
+    private AnimatedSprite _bat;
+
+    public Game1() : base(GameName, 1280, 720, false)
+    {
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        var atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+        _slime = atlas.CreateAnimatedSprite("slime-animation");
+        _slime.Scale = new Vector2(4.0f, 4.0f);
+
+        _bat = atlas.CreateAnimatedSprite("bat-animation");
+        _bat.Scale = new Vector2(4.0f, 4.0f);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        _slime.Update(gameTime);
+        _bat.Update(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+        _slime.Draw(SpriteBatch, Vector2.One);
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ SaintGameLibrary/Core.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace SaintGameLibrary;
+
+public class Core : Game
+{
+    internal static Core s_instance;
+
+    public static Core Instance => s_instance;
+
+    public static GraphicsDeviceManager Graphics { get; private set; }
+
+    public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+    public static SpriteBatch SpriteBatch { get; private set; }
+
+    public static new ContentManager Content { get; private set; }
+
+    /// <summary>
+    /// Creates a new Core instance.
+    /// </summary>
+    /// <param name="title">The title to display in the title bar of the game window.</param>
+    /// <param name="width">The initial width, in pixels, of the game window.</param>
+    /// <param name="height">The initial height, in pixels, of the game window.</param>
+    /// <param name="fullScreen">Indicates if the game should start in fullscreen mode.</param>
+    public Core(string title, int width, int height, bool fullScreen)
+    {
+        if (s_instance != null)
+        {
+            throw new InvalidOperationException($"Only a single Core instance can be created");
+        }
+
+        s_instance = this;
+        Graphics = new GraphicsDeviceManager(this);
+        Graphics.PreferredBackBufferWidth = width;
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.IsFullScreen = fullScreen;
+        Graphics.ApplyChanges();
+        Window.Title = title;
+        Content = base.Content;
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        GraphicsDevice = base.GraphicsDevice;
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+}
